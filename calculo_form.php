ec800f631c59f3b41b8962c1193995993e3b6eb1
carlejt <carlos.ejt@hotmail.com>
2015-02-26 23:00:45 -0500

    Create calculo_form.php

---
+++ calculo_form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<b> Calculo (PRODUCTORIA) </b><br>
+<a href="index.html"> Inicio </a> <br><br><br>
+
+<form name="formulario" action="calculo_submit.php" method="post">
+Numero = 
+<input type="text" name="numero">
+<input type="submit" value="Calcular">
+</form>
+
+<br><br>
+Test de Nube! creado por <b><i>Carlos E. Jimenez</i></b>
+
+
+
+
+
+</body>
+</html>
