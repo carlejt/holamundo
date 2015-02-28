50cfd26e2de4e3892e21add973ff9bdce1a65f9c
carlejt <carlos.ejt@hotmail.com>
2015-02-27 19:23:26 -0500

    Update conection.php

--- conection.php
+++ conection.php
@@ -1,9 +1,1 @@
-<?php
 
-function ConnectToDB(){
-    $conexion = mysql_connect($_ENV["OPENSHIFT_DB_HOST"] . ":3306", "adminfcYCXwx", "d8tYKbFbPfvt");
-    mysql_select_db("AgendaDB", $conexion);		
-	return $conexion;
-}
-
-?>
