8f5470c7f9430aea771337fb9bbf2d76ac13ddc5
carlejt <carlos.ejt@hotmail.com>
2015-02-27 19:25:15 -0500

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,19 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
 
-<?php
-Include_once("conection.php");
-
-$id = $_GET["id"];
-
-$conexion = ConnectToDB();
-   mysql_query("Delete From Agenda Where id = " . $id . ";", $conexion);
-mysql_close($conexion);
-
-header('Location: crud.php');
-
-?>
-
-</body>
-</html>
